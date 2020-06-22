6f6159ba1680be776464417c3988c39a64b5a2fc
Al <hoplla18@gmail.com>
2020-06-22 17:06:31 +0300

    Lesson2 testPUT, testPATCH, testDELETE were finished

--- src/test/java/Demo/Lesson2.java
+++ src/test/java/Demo/Lesson2.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.given;
 
@@ -32,7 +33,7 @@ public class Lesson2 {
         System.out.println(request);
 
         given()
-                .header("Content-type","application/json")
+                .header("Content-type", "application/json")
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
                 .body(request.toJSONString()).
@@ -41,4 +42,50 @@ public class Lesson2 {
                 then()
                 .statusCode(201);
     }
+
+    @Test
+    public void testPUT() {
+
+        JSONObject request = new JSONObject();
+        request.put("name", "Lera");
+        request.put("job", "logistics");
+
+        System.out.println(request);
+
+        given()
+                .body(request.toJSONString()).
+                when()
+                .put("https://reqres.in/api/users/2").
+                then()
+                .statusCode(200)
+                .log().all();
+    }
+
+    @Test
+    public void testPATCH() {
+
+        JSONObject request = new JSONObject();
+        request.put("job", "logistics");
+
+        System.out.println(request);
+
+        given()
+                .body(request.toJSONString()).
+                when()
+                .patch("https://reqres.in/api/users/2").
+                then()
+                .statusCode(200)
+                .log().all();
+    }
+
+    @Test
+    public void testDELETE() {
+
+        when()
+                .delete("https://reqres.in/api/users/2").
+                then()
+                .statusCode(204)
+                .log().all();
+    }
+
 }
